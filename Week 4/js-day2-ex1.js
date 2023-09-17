0b9b7455f8a804497b812e07b1307b3f1b2a27a8
paul7299 <paul7299@stud.kea.dk>
2023-09-17 15:22:56 +0200

    almost done

--- Week 4/js-day2-ex1.js
+++ Week 4/js-day2-ex1.js
@@ -3,14 +3,15 @@
 
 // Get all div elements on the page
     var divs = document.querySelectorAll("div");
+    
     for (var i = 0; i < divs.length; i++) {
-        divs[i].style.backgroundColor = getRandomColor(); // Replace with your chosen color
+        divs[i].style.backgroundColor = "blue"; // Replace with your chosen color
     }
 
-    // Get the button element
+// Assign the button element to a js variable
     var button = document.getElementById("changeColorButton");
     
-    // Function to generate a random color
+// Function to generate a random color
     function getRandomColor() {
         var letters = "0123456789ABCDEF";
         var color = "#";
@@ -20,20 +21,21 @@
         return color;
     }
 
+// function to loop thorugh and change the current div's color
     function colorchange() {
         // Loop through the divs and assign each a unique color
             for (var i = 0; i < divs.length; i++) {
-                var uniqueColor = getRandomColor();
-                divs[i].style.backgroundColor = uniqueColor;
+                divs[i].style.backgroundColor = getRandomColor();
             }
         }
 
-
+// Adding eventlistener to the button
+// with parameter "click" and the function I made before
     button.addEventListener("click", colorchange
         );
 
 
-        // ekstra
+// Adding the same eventListener to the divs as well
     for (var i = 0; i < divs.length; i++) {
         divs[i].addEventListener("click", colorchange
         );
